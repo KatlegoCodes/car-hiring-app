d5aa3549a833c9c56c0b81b3e353b44a661c8cfc
Frost <katlegcodes@gmail.com>
2026-07-24 15:21:04 +0200

    feat: update Prisma configuration to ensure DATABASE_URL is non-null and improve seed script connection handling

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,9 +1,16 @@
 import "dotenv/config";
 import { PrismaClient } from "../src/generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL!,
+});
 
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
-const prisma = new PrismaClient({ adapter });
+export const prisma = new PrismaClient({
+  adapter: new PrismaPg(pool),
+});
 
 const cars = [
   {
